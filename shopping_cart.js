a70bc25532f607bab95dc6a6406e4247f2959f90
Kirsten David <Kirsten.D328@gmail.com>
2022-06-16 16:20:22 -0500

    first commit

---
+++ shopping_cart.js
@@ -0,0 +1,24 @@
+// variables
+// var legacyVariable="shop"
+// let mutableVariable="some variable"
+// const immutableVariable="platinum"
+// legacyVariable="silver"
+// mutableVariable="gold"
+// immutableVariable="platinum"
+
+// data types
+const string="hello world"
+const number=7
+const boolean=true
+const nullValue=null
+const list= ["hello world",7,true,null]
+const object= {string:"hello world",number:7,boolean:true,nullValue:null}
+console.log(string,number,boolean,nullValue)
+
+function addTwoNumbers(num1,num2) {
+   return num1+num2
+}
+
+const sum=addTwoNumbers(3,8)
+
+console.log (sum)
